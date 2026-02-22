acf38352199ee484e81dd95b457a7cec59be5f57
L.B.R. <lbr@mmonad.com>
2026-02-22 19:52:12 +0000

    fix(zmosh): update sha256 hashes for v0.5.2

--- zmosh.rb
+++ zmosh.rb
@@ -7,18 +7,18 @@ class Zmosh < Formula
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/mmonad/zmosh/releases/download/v0.5.2/zmosh-0.5.2-macos-aarch64.tar.gz"
-      sha256 "49f2255be12247264a02ea109c993e4e1c706dcad1768520be5045316f1886be"
+      sha256 "891f4960118e339ffbb80b0e98b11a432bdae2ac362c965e1e7662668936a9bf"
     else
       url "https://github.com/mmonad/zmosh/releases/download/v0.5.2/zmosh-0.5.2-macos-x86_64.tar.gz"
-      sha256 "cbad932c109cd979f8c9539913f8ba8f930777e0ad87a59984e8b01cbaebeaec"
+      sha256 "4c8050166bb2b9ef33a6b33e53c965d7b5d4b2105542df289b3f00684def9ace"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
       url "https://github.com/mmonad/zmosh/releases/download/v0.5.2/zmosh-0.5.2-linux-aarch64.tar.gz"
-      sha256 "86c962c7452d9d1478c2aedcba0985c972419e8e05c64cb5c4958d6de27a4042"
+      sha256 "877af8aad81d6413393a203866ffa65fa3d22147845f419e27fbc9bc1f3fecda"
     else
       url "https://github.com/mmonad/zmosh/releases/download/v0.5.2/zmosh-0.5.2-linux-x86_64.tar.gz"
-      sha256 "70ea220e9b077cc6eba7b1a905f19bd303f1dd40f2ebf9d3519a1512c0b554a3"
+      sha256 "276efcadc56efe32ad78b1e9345f96b210055f87c3d11bfe9252bc9275251774"
     end
   end
 
