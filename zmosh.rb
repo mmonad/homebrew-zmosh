9a01d0abb5b0bd3dda9dd4e3d64abe081aff1e0f
L.B.R. <lbr@mmonad.com>
2026-02-22 17:53:49 +0000

    chore(zmosh): update formula to v0.5.2

--- zmosh.rb
+++ zmosh.rb
@@ -1,24 +1,24 @@
 class Zmosh < Formula
   desc "Session persistence with auto-reconnect for terminal processes"
   homepage "https://github.com/mmonad/zmosh"
-  version "0.5.1"
+  version "0.5.2"
   license "MIT"
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/mmonad/zmosh/releases/download/v0.5.1/zmosh-0.5.1-macos-aarch64.tar.gz"
-      sha256 "eb8e08183fb291762f11a5abfb183705000f68bf66d0aa57566580879e839266"
+      url "https://github.com/mmonad/zmosh/releases/download/v0.5.2/zmosh-0.5.2-macos-aarch64.tar.gz"
+      sha256 "49f2255be12247264a02ea109c993e4e1c706dcad1768520be5045316f1886be"
     else
-      url "https://github.com/mmonad/zmosh/releases/download/v0.5.1/zmosh-0.5.1-macos-x86_64.tar.gz"
-      sha256 "8f17a2448fcc216252f20e35fe4606c31b90c0f71fb246f5269dc132569ccc63"
+      url "https://github.com/mmonad/zmosh/releases/download/v0.5.2/zmosh-0.5.2-macos-x86_64.tar.gz"
+      sha256 "cbad932c109cd979f8c9539913f8ba8f930777e0ad87a59984e8b01cbaebeaec"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
-      url "https://github.com/mmonad/zmosh/releases/download/v0.5.1/zmosh-0.5.1-linux-aarch64.tar.gz"
-      sha256 "cab29d242557b31752df976619f8812d569e2ec10fdc6d3c1e0978064fa63ab9"
+      url "https://github.com/mmonad/zmosh/releases/download/v0.5.2/zmosh-0.5.2-linux-aarch64.tar.gz"
+      sha256 "86c962c7452d9d1478c2aedcba0985c972419e8e05c64cb5c4958d6de27a4042"
     else
-      url "https://github.com/mmonad/zmosh/releases/download/v0.5.1/zmosh-0.5.1-linux-x86_64.tar.gz"
-      sha256 "d1515398eb874f73ac9d251da20f005f9b7dd744f7565eb15ca28d6436221948"
+      url "https://github.com/mmonad/zmosh/releases/download/v0.5.2/zmosh-0.5.2-linux-x86_64.tar.gz"
+      sha256 "70ea220e9b077cc6eba7b1a905f19bd303f1dd40f2ebf9d3519a1512c0b554a3"
     end
   end
 
